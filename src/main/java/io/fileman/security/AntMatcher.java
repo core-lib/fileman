ad849614dda81d4b6f0c407992ad55acb670b122
杨昌沛 <646742615@qq.com>
2018-09-25 18:01:43 +0800

    1. 实现安全认证

--- src/main/java/io/fileman/security/AntMatcher.java
+++ src/main/java/io/fileman/security/AntMatcher.java
@@ -8,7 +8,18 @@ package io.fileman.security;
  */
 public class AntMatcher implements Matcher {
 
-    // /**/abc/def/**/*.jsp
+    /**
+     * 文本与ant表达式匹配
+     * 情况：
+     * 1. 路径中没有层级通配符
+     * 2. 路径中包含层级通配符
+     * 2.1. 层级通配符在最前面
+     * 2.2. 层级通配符在中间
+     *
+     * @param text    文本
+     * @param pattern 表达式
+     * @return
+     */
     @Override
     public boolean matches(String text, String pattern) {
         String[] segments = pattern.split("/");
